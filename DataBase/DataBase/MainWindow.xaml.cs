8aaa1ee2e8a92aa7a964778329cf6b628c3b7a59
Maxim-Veretin <maxim.veretin.2410@gmail.com>
2019-05-23 11:27:22 +0700

    dbadd+

--- DataBase/DataBase/MainWindow.xaml.cs
+++ DataBase/DataBase/MainWindow.xaml.cs
@@ -19,27 +19,51 @@ namespace DataBase
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public class DataBase
-    {
-        public int num { get; set; }
-        public string name { get; set; }
-        public int phys { get; set; }
-        public int math { get; set; }
-    }
+
 
     public partial class MainWindow : Window
     {
+
+        public class DataBase
+        {
+            public int num { get; set; }
+            public string name { get; set; }
+            public int phys { get; set; }
+            public int math { get; set; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
 
-            SQLiteConnection.CreateFile("D:\\Users\\Admin\\source\\repos\\РИ-88\\DataBase\\db_kek.db");
         }
+
+        SQLiteConnection m_dbConnection;
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
-            var data = new DataBase { num = int.Parse(reader["num"].ToString()), name = reader["name"].ToString(), phys = int.Parse(reader["pyhs"].ToString()), math = int.Parse(reader["math"].ToString()) };
+            //Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            //dlg.ShowDialog();
+
+            ////имя базы данных
+            //string db1 = dlg.FileName;
+
+            //SQLiteConnection m_dbConnection;
+            //m_dbConnection = new SQLiteConnection("Data Source = " + db1 + ";Version = 3");
+
+            ////открытие соединения с базой даных
+            //m_dbConnection.Open();
+
+            string sql = "INSERT INTO score,name (№, Ф.И.О., Физика, Матеша) VALUES (" + num.Text + ",'" + name.Text + ",'" + ph.Text + ",'" + mth.Text + "')";
+            //SQLiteConnection m_dbConnection;
+            //m_dbConnection = new SQLiteConnection("Data Source = " + db + ";Version = 1");
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            //SQLiteDataReader reader = command.ExecuteReader();
+
+            var data = new DataBase { num = int.Parse(num.Text.ToString()), name = name.Text.ToString(), phys = int.Parse(ph.Text.ToString()), math = int.Parse(mth.Text.ToString()) };
             db.Items.Add(data);
+            /*SQLiteDataReader reader = */command.ExecuteReader();
+            m_dbConnection.Close();
         }
 
         private void Red_Click(object sender, RoutedEventArgs e)
@@ -52,21 +76,29 @@ namespace DataBase
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Base_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.ShowDialog();
 
             //имя базы данных
-            string db = dlg.FileName;
+            string db1 = dlg.FileName;
 
             SQLiteConnection m_dbConnection;
-            m_dbConnection = new SQLiteConnection("db_kek" + db + ";Version=1");
+            m_dbConnection = new SQLiteConnection("Data Source = " + db1 + ";Version = 3");
 
             //открытие соединения с базой даных
             m_dbConnection.Open();
 
-            string sql = "SELECT * FROM db_kek ORDER BY "
+            string sql = "SELECT * FROM score,name WHERE score.№=name.№ ORDER BY №";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var data = new DataBase { num = int.Parse(reader["№"].ToString()), name = reader["Ф.И.О."].ToString(), phys = int.Parse(reader["Физика"].ToString()), math = int.Parse(reader["Матеша"].ToString()) };
+                db.Items.Add(data);
+            }
 
             m_dbConnection.Close();
         }
